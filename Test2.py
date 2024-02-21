73ae24f6b1fd141807214869cd3a1104ae9203de
darksward27 <joelapeter82@gmail.com>
2024-02-21 08:11:49 +0530

    Added Code

--- Test2.py
+++ Test2.py
@@ -1,24 +1,32 @@
-import numpy as np
-from Pyfhel import Pyfhel, PyPtxt, PyCtxt
+import csv
+from Pyfhel import Pyfhel, PyCtxt 
 
-# Create a Pyfhel object
-HE = Pyfhel()
+bfv_params = {
+    'scheme': 'BFV',    # can also be 'bfv'
+    'n': 2**13,         # Polynomial modulus degree, the num. of slots per plaintext,
+                        #  of elements to be encoded in a single ciphertext in a
+                        #  2 by n/2 rectangular matrix (mind this shape for rotations!)
+                        #  Typ. 2^D for D in [10, 16]
+    't': 65537,         # Plaintext modulus. Encrypted operations happen modulo t
+                        #  Must be prime such that t-1 be divisible by 2^N.
+    't_bits': 20,       # Number of bits in t. Used to generate a suitable value
+                        #  for t. Overrides t if specified.
+    'sec': 128,         # Security parameter. The equivalent length of AES key in bits.
+                        #  Sets the ciphertext modulus q, can be one of {128, 192, 256}
+                        #  More means more security but also slower computation.
+}
 
-# Initialize context with a prime modulus, a security level, and an encryption scheme
-HE.contextGen(65537, 128, "HElib")
+def encrypt_data( data):
+        HE = Pyfhel()
+        HE.contextGen(**bfv_params)
+        HE.keyGen()
+        encrypted_data = []
+        for d in data:
+            ctxt = PyCtxt()
+            HE.encrypt(ctxt, d)
+            encrypted_data.append(ctxt)
+        return encrypted_data
 
-# Generate encryption keys
-HE.keyGen()
+hu = encrypt_data(23)
 
-# Sample dataset (assuming it's a list of integers)
-dataset = [10, 20, 30, 40, 50]
-
-# Encrypt the dataset
-encrypted_dataset = []
-for x in dataset:
-    ptxt = PyPtxt([x], HE)
-    ctxt = HE.encrypt(ptxt)
-    encrypted_dataset.append(ctxt)
-
-# Print the encrypted dataset
-print("Encrypted dataset:", encrypted_dataset)
+print(hu)
